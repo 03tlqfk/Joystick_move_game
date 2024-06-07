c19d66761fa1174238ab796dbc47352978a3526f
03tlqfk <03tlqfk@gmail.com>
2024-06-07 15:52:33 +0900

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WindowsForm_move
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureObstacle1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureFlag = new System.Windows.Forms.PictureBox();
+            this.pictureTrap1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureObstacle1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureFlag)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureTrap1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureObstacle1
+            // 
+            this.pictureObstacle1.Image = global::WindowsForm_move.Properties.Resources.brick;
+            this.pictureObstacle1.Location = new System.Drawing.Point(155, -4);
+            this.pictureObstacle1.Name = "pictureObstacle1";
+            this.pictureObstacle1.Size = new System.Drawing.Size(100, 350);
+            this.pictureObstacle1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureObstacle1.TabIndex = 2;
+            this.pictureObstacle1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::WindowsForm_move.Properties.Resources.charactor;
+            this.pictureBox1.Location = new System.Drawing.Point(52, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureFlag
+            // 
+            this.pictureFlag.BackColor = System.Drawing.Color.Transparent;
+            this.pictureFlag.Image = global::WindowsForm_move.Properties.Resources.flag;
+            this.pictureFlag.Location = new System.Drawing.Point(760, 370);
+            this.pictureFlag.Name = "pictureFlag";
+            this.pictureFlag.Size = new System.Drawing.Size(50, 50);
+            this.pictureFlag.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureFlag.TabIndex = 1;
+            this.pictureFlag.TabStop = false;
+            // 
+            // pictureTrap1
+            // 
+            this.pictureTrap1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureTrap1.Image = global::WindowsForm_move.Properties.Resources.lurk_trap_;
+            this.pictureTrap1.Location = new System.Drawing.Point(683, 241);
+            this.pictureTrap1.Name = "pictureTrap1";
+            this.pictureTrap1.Size = new System.Drawing.Size(80, 50);
+            this.pictureTrap1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureTrap1.TabIndex = 3;
+            this.pictureTrap1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsForm_move.Properties.Resources.grass;
+            this.ClientSize = new System.Drawing.Size(884, 511);
+            this.Controls.Add(this.pictureTrap1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureFlag);
+            this.Controls.Add(this.pictureObstacle1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureObstacle1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureFlag)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureTrap1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureFlag;
+        private System.Windows.Forms.PictureBox pictureObstacle1;
+        private System.Windows.Forms.PictureBox pictureTrap1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WindowsForm_move
+{
+
+    public partial class Form1 : Form
+    {
+        private int position_x = 0;
+        private int position_y = 0;
+        private int I_Would_Win = 0;
+
+        private bool check_coll_left = false;
+        private bool check_coll_right = false;
+        private bool check_coll_top = false;
+        private bool check_coll_bottom = false;
+
+        private SerialPort serialPort;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeSerialPort();
+        }
+        private void InitializeSerialPort()
+        {
+            serialPort = new SerialPort("COM9", 9600); // 포트와 속도는 사용 중인 시리얼 장치에 맞게 수정해야 합니다.
+            serialPort.DataReceived += SerialPort_DataReceived;
+            try
+            {
+                serialPort.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("시리얼 포트를 열 수 없습니다: " + ex.Message);
+            }
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            try
+            {
+                string data = sp.ReadLine();
+                string[] values = data.Split(',');
+                if (values.Length == 2)
+                {
+                    char x = values[0][0];  //serial x값
+                    char y = values[1][0];  //serial y값
+
+
+                    //move
+                    if(x == '+')
+                    {
+                        position_x = position_x + 1;
+                    }
+                    else if(x == '*')
+                    {
+                        position_x = position_x + 2;
+                    }
+                    else if (x == '-')
+                    {
+                        position_x = position_x - 1;
+                    }
+                    else if (x == '/')
+                    {
+                        position_x = position_x - 2;
+                    }
+
+                    if (y == '+')
+                    {
+                        position_y = position_y + 1;
+                    }
+                    else if (y == '*')
+                    {
+                        position_y = position_y + 2;
+                    }
+                    else if (y == '-')
+                    {
+                        position_y = position_y - 1;
+                    }
+                    else if (y == '/')
+                    {
+                        position_y = position_y - 2;
+                    }
+
+                    //limit screen move
+                    if (position_x < 0)
+                    {
+                        position_x = 0;
+                    }
+                    if (position_y < 0)
+                    {
+                        position_y = 0;
+                    }
+                    if (position_x > 800)
+                    {
+                        position_x = 800;
+                    }
+                    if (position_y > 470)
+                    {
+                        position_y = 470;
+                    }
+
+                    //limit obstacel move
+                    if (pictureBox1.Bounds.IntersectsWith(pictureObstacle1.Bounds))
+                    {
+                        Rectangle intersect = Rectangle.Intersect(pictureBox1.Bounds, pictureObstacle1.Bounds);
+
+                        // 충돌 영역의 크기와 위치
+                        int intersectWidth = intersect.Width;
+                        int intersectHeight = intersect.Height;
+                        int intersectX = intersect.X - pictureBox1.Left;
+                        int intersectY = intersect.Y - pictureBox1.Top;
+
+                        // 충돌한 면 결정
+                        if (intersectWidth > intersectHeight)
+                        {
+                            if (intersectY > (pictureBox1.Height - intersectY))
+                            {
+                                // 위쪽 충돌
+                                Console.WriteLine("Top collision");
+                                position_y -= 2;
+                            }
+                            else
+                            {
+                                // 아래쪽 충돌
+                                Console.WriteLine("Bottom collision");
+                                position_y += 2;
+                            }
+                        }
+                        else
+                        {
+                            if (intersectX > (pictureBox1.Width - intersectX))
+                            {
+                                // 왼쪽 충돌
+                                Console.WriteLine("Left collision");
+                                position_x -= 2;
+                            }
+                            else
+                            {
+                                // 오른쪽 충돌
+                                Console.WriteLine("Right collision");
+                                position_x += 2;
+                            }
+                        }
+
+                    }
+
+                    //처음으로 가는 함정
+                    if(pictureBox1.Bounds.IntersectsWith(pictureTrap1.Bounds))
+                    {
+                        position_x = 0;
+                        position_y = 0;
+                    }
+
+                        MoveRectangle(position_x, position_y);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("데이터를 읽을 수 없습니다: " + ex.Message);
+            }
+        }
+
+        private void MoveRectangle(int x, int y)
+        {
+            if (pictureBox1.InvokeRequired)
+            {
+                pictureBox1.Invoke(new MethodInvoker(delegate { MoveRectangle(x, y); }));
+            }
+            else
+            {
+                pictureBox1.Location = new System.Drawing.Point(x, y);
+            }
+
+            
+
+            //win
+            if (pictureBox1.Bounds.IntersectsWith(pictureFlag.Bounds))
+            {
+                I_Would_Win++;
+            }
+            if(I_Would_Win  == 1)
+            {
+                I_Would_Win++;
+                MessageBox.Show("Win!");
+            }
+
+            /*
+            Console.Write(pictureFlag.Location.X);
+            Console.Write(",");
+            Console.Write(pictureFlag.Location.Y);
+            Console.Write("/");
+            Console.Write(pictureFlag.Size.Height);
+            Console.Write(",");
+            Console.WriteLine(pictureFlag.Size.Width);
+            */
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+        }
+    }
+}
